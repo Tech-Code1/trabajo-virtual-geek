9e947900d22f6d8f098d33754a51de44de7316da
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-17 13:59:57 -0500

    Reto 6 y 7

---
+++ 1-programacion-uno/reto-6.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* const divText = document.querySelector(".content-h1-div"); */
+
+const contentDiv = "<h1>Lorem ipsum</h1>"
+'<img src="http://via.placeholder.com/350x150">Lorem ipsum</img>'
+'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+document.querySelector(".content-h1-div").innerHTML = `${contentDiv}`;
---
+++ 1-programacion-uno/reto-7.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+const contentUl = document.querySelector(".list-two")
+
+contentUl.innerHTML = '<li>1</li> <li>2</li> <li>3</li>'
