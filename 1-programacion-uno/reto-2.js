ef86cb8b9a02ef13fa3476a3b57561e7a9bd5cba
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-17 11:57:02 -0500

    Reto 2

---
+++ 1-programacion-uno/reto-2.js
@@ -0,0 +1,14 @@
+/* 'use strict';
+
+const miDireccion = "calle 40 # 25 - 20"
+
+const miDireccion = "calle 40s # 17 - 18" */
+
+/* Uncaught TypeError: Assignment to constant variable.
+    at reto-2.js:5 */
+
+/* ***************Solucion*************** */
+
+const miDireccion = "calle 40 # 25 - 20"
+
+const tuDireccion = "calle 40s # 17 - 18"
