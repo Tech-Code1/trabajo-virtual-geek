c4e023dd118fdf872397ced4579e2322b78e6c51
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-17 21:57:25 -0500

    Ejercicio 2-4

---
+++ Fundamentos de Programación Parte Dos/ejercicio-4.js
@@ -0,0 +1,10 @@
+const totalCuenta = 128
+const personas = 9
+
+const cuentaDividida = totalCuenta / personas;
+
+const adicionAna = 2
+const totalAna = cuentaDividida + adicionAna;
+
+console.log(`cada persona tiene que pagar ${cuentaDividida}€, 
+Ana tiene que pagar un poco mas y su total es de ${totalAna}€`)
