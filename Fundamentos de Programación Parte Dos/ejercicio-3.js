ed9d58d45d320c8378e96cca241fa50dceaae821
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-17 21:41:47 -0500

    ejercicio 2-3

---
+++ Fundamentos de Programación Parte Dos/ejercicio-3.js
@@ -0,0 +1,12 @@
+const kiloKiwi = 2;
+const kiloPeras = 3;
+const kiloUvas = 0.5;
+
+
+const valorKiloKiwi = 5;
+const valorkiloPeras = 2;
+const valorKiloUvas = 4
+
+console.log(`El valor total del kiwi es de ${kiloKiwi * valorKiloKiwi}€,
+el valor total de las peras es de ${kiloPeras * valorkiloPeras}€
+y el valor total de las uvas es de ${kiloUvas * valorKiloUvas}€`);
