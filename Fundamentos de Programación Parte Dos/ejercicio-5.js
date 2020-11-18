1be3f9f7d451754846afc0c61391fd49fda923bf
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-17 23:17:31 -0500

    Ejercicio 2-5

---
+++ Fundamentos de Programación Parte Dos/ejercicio-5.js
@@ -0,0 +1,8 @@
+const año = 365
+const diaEnHoras = 24
+const edadDavid = 27
+
+const añoEnHoras = año * diaEnHoras;
+const vidaHoras = edadDavid * añoEnHoras;
+
+console.log(`he vivido ${vidaHoras} horas.`)
